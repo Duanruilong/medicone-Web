0d703dac69af6ec1959644041a70e7aae0bf7f9a
drlong <drl1210@163.com>
2018-07-02 00:13:07 +0800

    调试一下

--- src/services/needList.js
+++ src/services/needList.js
@@ -2,7 +2,7 @@
 import $ from '../utils/ajax';
 // 需求列表
 async function needListquery(params) {
-  return $.post('/medicone/requirement/getList',params);
+//   return $.post('/medicone/requirement/getList',params);
 }
 // 发布需求
 async function insertList(params) {
